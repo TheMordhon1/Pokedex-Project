3285a8273c4adacaff53a173cb4709df93310027
themordhon <romdhonm12@gmail.com>
2024-07-28 00:43:10 +0700

    membuat komponen InputText dan memperbaiki Navbar dan routing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import useNavigation from "../hooks/useNavigation";
+import InputText from "./InputText";
 
 const Navbar = () => {
   const { handleBackTo } = useNavigation();
+  const navigate = useNavigate();
+
+  let { menu } = useParams();
+
+  const [search, setSearch] = useState("");
+
+  const handleSubmitSearch = (e) => {
+    e.preventDefault();
+    navigate(`/${menu}?q=${search}`);
+  };
+
   return (
-    <header className="flex items-center justify-between w-full bg-ligthblack/50 border-grey/20 border-b h-[60px] px-8">
+    <header className="flex items-center justify-between w-full bg-ligthblack/50 border-grey/20 border-b h-[60px] px-8 md:px-20">
       <div
         className="flex items-center gap-2 cursor-pointer"
         onClick={() => handleBackTo("/")}
@@ -15,6 +28,13 @@ const Navbar = () => {
           POKÉDEX
         </div>
       </div>
+
+      <form onSubmit={handleSubmitSearch} className="">
+        <InputText
+          placeholder="🔍 Search pokémon"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </form>
     </header>
   );
 };
