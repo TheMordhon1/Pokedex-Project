b2250ce0ab8e2885fc56ad296d54e09482145230
themordhon <romdhonm12@gmail.com>
2024-07-30 00:05:32 +0700

    handle user log, change folder structure

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,40 +1,82 @@
 import React, { useState } from "react";
 import useNavigation from "../hooks/useNavigation";
 import InputText from "./InputText";
+import { FaUser } from "react-icons/fa";
+import { IoMdArrowDropdown } from "react-icons/io";
+import Swal from "sweetalert2";
 
 const Navbar = () => {
   const { handleBackTo, handleNavigateTo } = useNavigation();
+  const username = localStorage.getItem("username");
   const [search, setSearch] = useState("");
+  const [showLogout, setShowLogout] = useState(false);
 
   const handleSubmitSearch = (e) => {
     e.preventDefault();
     handleNavigateTo(`/pokemon/${search}`);
   };
 
+  const handleLogout = () => {
+    Swal.fire({
+      title: "Do you want to leave?",
+      showCancelButton: true,
+      confirmButtonText: "Yes",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        localStorage.clear();
+        handleBackTo("/");
+        setIsLoading(false);
+      }
+    });
+  };
+
   return (
     <header className="sticky top-0 z-10 flex items-center justify-between w-full bg-ligthblack border-grey/20 border-b h-[60px] px-8 md:px-20">
-      <div
-        className="flex items-center gap-2 cursor-pointer"
-        onClick={() => handleBackTo("/")}
-      >
-        <img src="./logo.png" alt="logo" className="w-10" />
-        <div className="font-bold -skew-x-12 text-white mt-2 text-xl">
-          POKÉDEX
+      <div className="flex items-center gap-6">
+        <div
+          className="flex items-center gap-2 cursor-pointer"
+          onClick={() => handleBackTo("/")}
+        >
+          <img src="./logo.png" alt="logo" className="w-10" />
+          <div className="font-bold -skew-x-12 text-white mt-2 text-xl">
+            POKÉDEX
+          </div>
         </div>
+        <form onSubmit={handleSubmitSearch} className="w-[15rem]">
+          <InputText
+            placeholder="🔍 Search pokémon"
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </form>
       </div>
-      <button
-        className="bg-lightblack hover:bg-slate-900 text-white font-bold py-2 px-4 rounded"
-        onClick={() => handleNavigateTo("/favourite")}
-      >
-        Favourite
-      </button>
-
-      <form onSubmit={handleSubmitSearch} className="w-[15rem]">
-        <InputText
-          placeholder="🔍 Search pokémon"
-          onChange={(e) => setSearch(e.target.value)}
-        />
-      </form>
+      <ul className="flex items-center gap-4">
+        <li>
+          <button
+            className="bg-lightblack hover:bg-slate-900 text-white font-bold py-2 px-4 rounded"
+            onClick={() => handleNavigateTo("/favourite")}
+          >
+            Favourite
+          </button>
+        </li>
+        <li className="relative">
+          <button
+            className="bg-black text-white font-bold py-2 px-4 rounded flex items-center gap-4"
+            onClick={() => setShowLogout(!showLogout)}
+          >
+            <FaUser className="text-white text-xl" />
+            {username}
+            <IoMdArrowDropdown className="text-white text-sm" />
+          </button>
+          {showLogout && (
+            <button
+              className="absolute -bottom-12 right-0 bg-white text-black py-2 rounded-md px-8 transition ease-in duration-200"
+              onClick={handleLogout}
+            >
+              Logout
+            </button>
+          )}
+        </li>
+      </ul>
     </header>
   );
 };
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,26 +1,62 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TextH1 from "../components/TextH1";
 import InputText from "../components/InputText";
 import useNavigation from "../hooks/useNavigation";
 import { toast } from "react-toastify";
 import axios from "axios";
+import { FaCheckCircle } from "react-icons/fa";
 
 const Home = () => {
   const [name, setName] = useState("");
+  const userLog = localStorage.getItem("username");
 
   const { handleNavigateTo } = useNavigation();
+  const getUserName = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/user");
+
+      const username = response.data.filter((user) => user.username === name);
+
+      if (username[0]?.username === name) {
+        toast.success(`Welcome back, ${username[0].username}`, {
+          theme: "dark",
+          position: "top-right",
+          progressStyle: { backgroundColor: "rgba(237, 213, 63, 1)" },
+          icon: <FaCheckCircle className="text-3xl text-el_electric" />,
+        });
+      } else {
+        await axios.post(`http://localhost:3000/user`, {
+          username: name,
+        });
+        toast.success(`Welcome, ${name}`, {
+          theme: "dark",
+          position: "top-right",
+          progressStyle: { backgroundColor: "rgba(237, 213, 63, 1)" },
+          icon: <FaCheckCircle className="text-3xl text-el_electric" />,
+        });
+      }
+      handleNavigateTo(`/pokemon`);
+      localStorage.setItem("username", name);
+    } catch (error) {
+      console.log("error get username:", error);
+      toast.error(error.message, { theme: "dark" });
+    }
+  };
+
   const handleInputName = async (e) => {
     e.preventDefault();
     try {
-      await axios.post(`http://localhost:3000/user`, { username: name });
-      handleNavigateTo("/pokemon");
-      localStorage.setItem("username", name);
-      toast.success(`Hello ${name}`, { theme: "dark", hideProgressBar: true });
+      await getUserName();
     } catch (error) {
-      toast.error(error.message.response);
-      console.log(error);
+      console.log("error input user:", error);
     }
   };
+
+  useEffect(() => {
+    if (userLog) {
+      handleNavigateTo("/pokemon");
+    }
+  }, []);
   return (
     <section className="h-[calc(100vh-60px)] flex flex-col justify-center items-center">
       <img src="./logo.png" width={150} alt="" />
