cbc1aba721b04f7cf6a84c7984bfaac64442b8fa
themordhon <romdhonm12@gmail.com>
2024-07-30 07:14:47 +0700

    update navbar for mobile & create sidebar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,14 +2,16 @@ import React, { useState } from "react";
 import useNavigation from "../hooks/useNavigation";
 import InputText from "./InputText";
 import { FaUser } from "react-icons/fa";
-import { IoMdArrowDropdown } from "react-icons/io";
+import { IoMdArrowDropdown, IoMdMenu } from "react-icons/io";
 import Swal from "sweetalert2";
+import Sidebar from "./Sidebar";
 
 const Navbar = () => {
   const { handleBackTo, handleNavigateTo } = useNavigation();
   const username = localStorage.getItem("username");
   const [search, setSearch] = useState("");
   const [showLogout, setShowLogout] = useState(false);
+  const [sidebarShown, setSidebarShown] = useState(false);
 
   const handleSubmitSearch = (e) => {
     e.preventDefault();
@@ -30,9 +32,63 @@ const Navbar = () => {
     });
   };
 
+  const handleToggleMenu = () => {
+    setSidebarShown(!sidebarShown);
+  };
+
   return (
-    <header className="sticky top-0 z-10 flex items-center justify-between w-full bg-ligthblack border-grey/20 border-b h-[60px] px-8 md:px-20">
-      <div className="flex items-center gap-6">
+    <>
+      {/* Navbar Desktop */}
+      <header className="sticky top-0 z-10 items-center justify-between w-full bg-ligthblack border-grey/20 border-b h-[60px] px-8 md:px-20 hidden md:flex">
+        <div className="flex items-center gap-6">
+          <div
+            className="flex items-center gap-2 cursor-pointer"
+            onClick={() => handleBackTo("/")}
+          >
+            <img src="./logo.png" alt="logo" className="w-10" />
+            <div className="font-bold -skew-x-12 text-white mt-2 text-xl">
+              POKÉDEX
+            </div>
+          </div>
+          <form onSubmit={handleSubmitSearch} className="w-[15rem]">
+            <InputText
+              placeholder="🔍 Search pokémon"
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          </form>
+        </div>
+        <ul className="flex items-center gap-4">
+          <li>
+            <button
+              className="bg-lightblack hover:bg-slate-900 text-white font-bold py-2 px-4 rounded"
+              onClick={() => handleNavigateTo("/favourite")}
+            >
+              Favourite
+            </button>
+          </li>
+          <li className="relative">
+            <button
+              className="bg-black text-white font-bold py-2 px-4 rounded flex items-center gap-4"
+              onClick={() => setShowLogout(!showLogout)}
+            >
+              <FaUser className="text-white text-xl" />
+              {username}
+              <IoMdArrowDropdown className="text-white text-sm" />
+            </button>
+            {showLogout && (
+              <button
+                className="absolute -bottom-12 right-0 bg-white text-black py-2 rounded-md px-8 transition ease-in duration-200"
+                onClick={handleLogout}
+              >
+                Logout
+              </button>
+            )}
+          </li>
+        </ul>
+      </header>
+
+      {/* Navbar Mobile */}
+      <header className="sticky top-0 z-50 flex md:hidden items-center justify-between w-full bg-ligthblack border-grey/20 border-b h-[60px] px-8 md:px-20">
         <div
           className="flex items-center gap-2 cursor-pointer"
           onClick={() => handleBackTo("/")}
@@ -42,42 +98,26 @@ const Navbar = () => {
             POKÉDEX
           </div>
         </div>
-        <form onSubmit={handleSubmitSearch} className="w-[15rem]">
-          <InputText
-            placeholder="🔍 Search pokémon"
-            onChange={(e) => setSearch(e.target.value)}
-          />
-        </form>
-      </div>
-      <ul className="flex items-center gap-4">
-        <li>
-          <button
-            className="bg-lightblack hover:bg-slate-900 text-white font-bold py-2 px-4 rounded"
-            onClick={() => handleNavigateTo("/favourite")}
-          >
-            Favourite
-          </button>
-        </li>
-        <li className="relative">
-          <button
-            className="bg-black text-white font-bold py-2 px-4 rounded flex items-center gap-4"
-            onClick={() => setShowLogout(!showLogout)}
-          >
-            <FaUser className="text-white text-xl" />
-            {username}
-            <IoMdArrowDropdown className="text-white text-sm" />
-          </button>
-          {showLogout && (
-            <button
-              className="absolute -bottom-12 right-0 bg-white text-black py-2 rounded-md px-8 transition ease-in duration-200"
-              onClick={handleLogout}
-            >
-              Logout
-            </button>
-          )}
-        </li>
-      </ul>
-    </header>
+
+        <div className="flex items-center gap-4">
+          <form onSubmit={handleSubmitSearch} className="w-[15rem]">
+            <InputText
+              placeholder="🔍 Search pokémon"
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          </form>
+          <IoMdMenu className="text-white text-xl" onClick={handleToggleMenu} />
+        </div>
+      </header>
+
+      <Sidebar
+        username={username}
+        shown={sidebarShown}
+        handleToggle={handleToggleMenu}
+        handleSubmitSearch={handleSubmitSearch}
+        handleLogout={handleLogout}
+      />
+    </>
   );
 };
 
---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import TextH1 from "./TextH1";
+
+const Sidebar = ({ username, shown, handleToggle, handleLogout }) => {
+  return (
+    <>
+      <aside
+        className={`flex flex-col justify-between h-[calc(100vh-60px)] fixed z-50 w-[60%] bg-ligthblack border-l border-ligthblack/50 px-6 py-10 transition-all ease-linear duration-200 ${
+          shown ? "right-0" : "-right-[100%]"
+        }`}
+      >
+        <ul className="grid gap-4">
+          <li>
+            <button
+              className="bg-black/50 hover:bg-slate-900 text-white font-bold py-2 px-4 rounded w-full"
+              onClick={() => handleNavigateTo("/favourite")}
+            >
+              Favourite
+            </button>
+          </li>
+        </ul>
+        <div className="grid gap-4">
+          <h4 className="text-white font-semibold text-xl">
+            Hello, {username}
+          </h4>
+          <button
+            className="bg-el_fighting text-white font-semibold rounded-md py-4"
+            onClick={handleLogout}
+          >
+            Logout
+          </button>
+        </div>
+      </aside>
+      {shown && (
+        <div
+          className="bg-black/50 fixed z-20 top-0 left-0 right-0 bottom-0 w-full h-full"
+          onClick={handleToggle}
+        />
+      )}
+    </>
+  );
+};
+
+export default Sidebar;
